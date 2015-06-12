91e485516cf4d59d10cdf12e522f5e9bc984dd0f
saisagar1990 <kaches@chetu.com>
2015-06-12 16:10:58 +0530

    templage table name change

--- db/migrate/20150609103648_merge_report_header_to_report_template_master.rb
+++ db/migrate/20150609103648_merge_report_header_to_report_template_master.rb
@@ -1,14 +1,14 @@
 class MergeReportHeaderToReportTemplateMaster < ActiveRecord::Migration
   def change
-  	add_column :report_template_masters, :main_header, :string
-  	add_column :report_template_masters, :main_header_style, :string
-  	add_column :report_template_masters, :header1, :string
-  	add_column :report_template_masters, :header1_style, :string
-  	add_column :report_template_masters, :header2, :string
-  	add_column :report_template_masters, :header2_style, :string
-  	add_column :report_template_masters, :header3, :string
-  	add_column :report_template_masters, :header3_style, :string
-  	add_column :report_template_masters, :repeat_header, :boolean
+  	add_column :templates, :main_header, :string
+  	add_column :templates, :main_header_style, :string
+  	add_column :templates, :header1, :string
+  	add_column :templates, :header1_style, :string
+  	add_column :templates, :header2, :string
+  	add_column :templates, :header2_style, :string
+  	add_column :templates, :header3, :string
+  	add_column :templates, :header3_style, :string
+  	add_column :templates, :repeat_header, :boolean
 
   end
 end
